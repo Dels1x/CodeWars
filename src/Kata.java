5bf2b894d6e05141b527c184c711dbe198aa16d9
Danislav <delsixreborn@gmail.com>
2022-07-19 18:02:52 +0300

    CodeWars: Find the missing letter
    https://www.codewars.com/kata/5839edaa6754d6fec10000a2/train/java

---
+++ src/Kata.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Kata
+{
+    public static char findMissingLetter(char[] array)
+    {
+        int[] numbers = new String(array).chars().sorted().toArray();
+        System.out.println(Arrays.toString(numbers));
+        int pos = IntStream.range(1, numbers.length)
+                .filter(index -> (numbers[index] - numbers[index-1] != 1))
+                .findFirst().getAsInt();
+        return (char) (numbers[pos]-1);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Kata.findMissingLetter(new char[] { 'a','b','c','d','f' }));
+    }
+}
