e53cc6487b62d5e1b055d490605e14738827a0d4
Danislav <delsixreborn@gmail.com>
2022-07-21 12:34:16 +0300

    LeetCode: 2. i used linkedlists instead of nodelists lol

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,32 @@
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {3, 2, 3};
+        LinkedList<Integer> arr1 = new LinkedList<>();
+        LinkedList<Integer> arr2 = new LinkedList<>();
 
-        System.out.println(Arrays.toString(Solution.twoSum(arr, 6)));
+        arr1.add(2);
+        arr1.add(3);
+        arr1.add(6);
+        arr2.add(2);
+        arr2.add(3);
+        arr2.add(4);
+
+        System.out.println(Solution.addTwoNumbers(arr1, arr2));
 
 
     }
 }
 
 class Solution {
-    static public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = 0; j < nums.length; j++) {
-                if(i != j && nums[i] + nums[j] == target) {
-                    return new int[] {i, j};
-                }
-            }
+    public static LinkedList<Integer> addTwoNumbers(LinkedList<Integer> l1, LinkedList<Integer> l2) {
+        for(int i = 0; i < l1.size(); i++) {
+            l1.set(i, l1.get(i) + l2.get(i));
+            l1.set(i, Character.getNumericValue(String.valueOf(l1.get(i)).charAt(String.valueOf(l1.get(i)).length()-1)));
         }
 
-        return new int[2];
+        return l1;
     }
 }
 
