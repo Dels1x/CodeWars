d4a64f0c5fa38e34e42c1b2a7a12b203e87b2767
Danislav <delsixreborn@gmail.com>
2022-07-22 23:57:25 +0300

    CodeWars: <5kyu> Phone Directory 2/2 this kata is broken i give up
    https://www.codewars.com/kata/56baeae7022c16dd7400086e/train/java

--- src/PhoneDir.java
+++ src/PhoneDir.java
@@ -8,20 +8,31 @@ class PhoneDir {
         String[] lines = strng.split("\n");
         long count = Arrays.stream(lines).filter(i -> i.contains(num)).count();
         if(count > 1) {
-            return "Error => Too many people: num";
+            return "Error => Too many people: "+num;
         } else if (count < 1) {
-            return "Error => Not found: num";
+            return "Error => Not found: "+num;
         }
 
         StringBuilder output = new StringBuilder("Phone => ");
 
         for(String line: lines) {
             if(line.contains(num)) {
-                output.append(num);
-                output.append(",  Name => ");
-                for(int character = 0; character < line.length()-1; character++) {
+                System.out.println(line);
 
-                }
+                output.append(num).append(", Name => ");
+
+                line = line.replace(num, "");
+
+                String address =  findName(line, "<+[^\\<>]+>");
+
+                output.append(address).append(", Address => ");
+
+                line = line.replace("<"+address+">", "").replaceAll("[#@$%:^&*!?+/;,]", "").replaceAll("\\s+", " ").replace("_", " ");
+
+                output.append(line.trim());
+
+                break;
+
             }
         }
 
@@ -29,14 +40,22 @@ class PhoneDir {
 
     }
 
-    public static void printMatches(String text, String regex) {
+    public static String findName(String text, String regex) {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
-        // Check all occurrences
+
+        String output = "";
+        int start = 0;
+        int end = 0;
+
         while (matcher.find()) {
-            System.out.print("Start index: " + matcher.start());
-            System.out.print(" End index: " + matcher.end());
-            System.out.println(" Found: " + matcher.group());
+            start = matcher.start();
+            end = matcher.end();
         }
+
+        System.out.println(start);
+        System.out.println(end);
+
+        return text.substring(start + 1, end - 1);
     }
 }
