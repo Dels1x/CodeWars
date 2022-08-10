7c41c5e672069c01fe7237aaac100cfb349c0be2
Danislav <delsixreborn@gmail.com>
2022-08-10 23:25:30 +0300

    CodeWars: <7kyu> Disemvowel Trolls
    https://www.codewars.com/kata/52fba66badcd10859f00097e/train/java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Solution.whatCentury(2254));
-
+        System.out.println(Troll.disemvowel("This website is for losers LOL!"));
 
     }
 
+}
+
+class Troll {
+    public static String disemvowel(String str) {
+        return str.replaceAll("[aoiueAOIUE]", "");
+    }
 }
