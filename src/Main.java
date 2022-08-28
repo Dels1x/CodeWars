936912b967dec2ca8572dc38c56727daa55f8ca1
Danislav <delsixreborn@gmail.com>
2022-08-28 16:59:54 +0300

    CodeWars: <7kyu> Build a square
    https://www.codewars.com/kata/59a96d71dbe3b06c0200009c/train/java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Troll.disemvowel("This website is for losers LOL!"));
+        System.out.println(Kata2.generateShape(5));
 
     }
 
 }
 
-class Troll {
-    public static String disemvowel(String str) {
-        return str.replaceAll("[aoiueAOIUE]", "");
+class Kata2 {
+    public static String generateShape(int n) {
+        String output = "";
+
+        for(int i = 0; i < n; i++) {
+            output += "+".repeat(n).concat("\n");
+        }
+
+        return output.substring(0, output.length()-1);
     }
 }
