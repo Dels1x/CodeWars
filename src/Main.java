0a7e4ea07624a43fc413b2a1015cc17d044bb172
Danislav <delsixreborn@gmail.com>
2022-09-01 17:20:25 +0300

    CodeWars: <5kyu> RGB To Hex Conversion
    https://www.codewars.com/kata/513e08acc600c94f01000001/train/java

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(LongestConsec.longestConsec(new String[] {"tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"}, 2));
+        System.out.println(RgbToHex.rgb(1565, 265, 255));
 
     }
 }
 
-class LongestConsec {
-
-    public static String longestConsec(String[] strarr, int k) {
-        if(strarr.length == 0 || k > strarr.length || k < 0) {
-            return "";
-        }
-
-        int bestLength = 0;
-        String bestString = "";
-        String currentString;
-
-        for(int i = 0; i < strarr.length - k + 1; i++) {
-            currentString = "";
-            for(int j = 0; j < k; j++) {
-                currentString = currentString.concat(strarr[i+j]);
-            }
-
-            if(currentString.length() > bestLength) {
-                bestLength = currentString.length();
-                bestString = currentString;
-            }
-        }
-
-        return bestString;
-    }
-}
